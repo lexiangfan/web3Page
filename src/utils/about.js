d93cb806536dd2c60bf22248227e43a39ca05e62
qiYu <1322941394@qq.com>
2025-08-13 15:35:21 +0800

    web3

--- src/utils/about.js
+++ src/utils/about.js
@@ -16,6 +16,27 @@ import imKey_2_1_3 from '@/public/images/imKey_2_1_3.png'
 import imKey_2_1_4 from '@/public/images/imKey_2_1_4.png'
 import imKey_2_1_5 from '@/public/images/imKey_2_1_5.png'
 import imKey_2_1_6 from '@/public/images/imKey_2_1_6.png'
+import imKey_2_3_1 from '@/public/images/imKey_2_3_1.png'
+import imKey_2_4_1 from '@/public/images/imKey_2_4_1.png'
+import imKey_4 from '@/public/images/密盒 S1  VS  密盒 P1.png'
+import imKey_4_2_1 from '@/public/images/miheS1.png'
+import imKey_4_2_2 from '@/public/images/miheS1参数.png'
+import imKey_4_2_3 from '@/public/images/miheS1包装清单.png'
+import imKey_4_2_4 from '@/public/images/miheS1防火.png'
+import imKey_4_2_5 from '@/public/images/miheS1防水耐腐蚀.png'
+import imKey_4_2_6 from '@/public/images/miheS1防冲击.png'
+import imKey_4_2_7 from '@/public/images/miheS1可锁定防窥.png'
+import imKey_4_2_8 from '@/public/images/miheS1易于使用_恢复.png'
+import imKey_4_2_9 from '@/public/images/miheS1步骤1.png'
+import imKey_4_2_10 from '@/public/images/miheS1步骤2.png'
+import imKey_4_2_11 from '@/public/images/miheS1步骤3.png'
+import imKey_4_2_12 from '@/public/images/miheS1步骤4.png'
+import imKey_4_2_13 from '@/public/images/miheS1步骤5.png'
+import imKey_4_2_14 from '@/public/images/miheS1步骤6.png'
+import imKey_4_2_15 from '@/public/images/miheS1放入保险柜.png'
+import imKey_4_3_1 from '@/public/images/miheS1常见问题1.jpg'
+import imKey_4_3_2 from '@/public/images/miheS1常见问题2.jpg'
+import imKey_4_3_3 from '@/public/images/miheS1定制.png'
 // 页面内容模板
 export const about = [
     {
@@ -41,7 +62,7 @@ export const about = [
 <td>
 <p><strong>imKey Pro 硬件钱包</strong></p>
 <p>选用 CC EAL6+ 安全芯片，最高等级的安全保障，囤币首选；使用 imKey，你可以随时随地购买、出售和转移您的加密货币。</p>
-<p><strong>售价：¥ 688</strong></p>
+<p><strong>售价：¥ 738</strong></p>
 <p><a target="_blank" rel="noopener noreferrer" href="https://shop42404838.m.youzan.com/wscgoods/detail/2xj05mqphzjou2e?banner_id=f.107612083~goods.13~1~S3E60NgO&alg_id=common_by_external%2Cnot_need_rank%2C0.0.0.0.0.0.0.0.701_31a2bb913eda405c8e77028da74d246a&slg=0&components_style_layout=1&reft=1676614259726_1676614269708&spm=g.552622152_f.77421469_f.107612083">更多了解</a></p>
 </td>
 </tr>
@@ -50,7 +71,7 @@ export const about = [
 <td>
 <p><strong>密盒 S1</strong></p>
 <p>性价比最高的助记词保管器，防水、防火、防腐蚀、防冲击且完全离线，隔离一切潜在风险；</p>
-<p><strong>售价：¥ 249</strong></p>
+<p><strong>售价：¥ 279</strong></p>
 <p><a target="_blank" rel="noopener noreferrer" href="https://shop42404838.m.youzan.com/wscgoods/detail/2xj16xca3nkmm?dc_ps=3340495314934140935.200001">更多了解</a></p>
 </td>
 </tr>
@@ -331,8 +352,427 @@ export const about = [
                         </table>
 </div>
                         `
+                    },
+                    {
+                        id:'imKey-2-3',
+                        title:'imKey Pro 硬件钱包-基本参数',
+                        content:`
+                        <div class="table-container">
+                        <table class="table product-table">
+                        <tbody>
+                        <tr>
+                        <td rowspan="10"><img src="${imKey_2_3_1}" alt="imKey_2_3_1"></td>
+                        <td>主体重量</td>
+                        <td>8.1g</td>
+</tr>
+<tr>
+<td>设备尺寸</td>
+<td>64mmx38mmx2.3mm</td>
+</tr>
+<tr>
+<td>产品材质</td>
+<td>PC 面板 + 锌合金外壳</td>
+</tr>
+<tr>
+<td>产品颜色</td>
+<td>黑色</td>
+</tr>
+<tr>
+<td>连接方式</td>
+<td>蓝牙/USB</td>
+</tr>
+<tr>
+<td>电池类型</td>
+<td>锂离子聚合物电池</td>
+</tr>
+<tr>
+<td>输入电压</td>
+<td>DC 5.0 V</td>
+</tr>
+<tr>
+<td>电池容量</td>
+<td>33mAh</td>
+</tr>
+<tr>
+<td>工作温度</td>
+<td>0°C ～ 50°C</td>
+</tr>
+<tr>
+<td>产品升级</td>
+<td>支持固件升级</td>
+</tr>
+</tbody>
+                        </table>
+                        </div>
+                        `
+                    },
+                    {
+                        id:'imKey-2-4',
+                        title:'imKey Pro 硬件钱包-包装清单',
+                        content:`
+                        <div class="table-container">
+                        <table class="table product-table">
+                        <tbody>
+                        <tr>
+                        <td rowspan="8"><img src="${imKey_2_4_1}" alt="imKey_2_4_1"></td>
+                        <td>包装重量</td>
+                        <td>214g</td>
+</tr>
+<tr>
+<td>包装尺寸</td>
+<td>100mmx100mmx30mm</td>
+</tr>
+<tr>
+<td rowspan="6">包装内容</td>
+<td>1 x 腰封</td>
+</tr>
+<tr>
+<td>1 x 天地盒盒体</td>
+</tr>
+<tr>
+<td>1 x 说明书</td>
+</tr>
+<tr>
+<td>1 x imKey Pro</td>
+</tr>
+<tr>
+<td>1 x 数据线</td>
+</tr>
+<tr>
+<td>2 x 助记词卡片</td>
+</tr>
+</tbody>
+                        </table>
+                        </div>
+                        `
                     }
                 ]
+            },
+            {
+                id:'imKey-3',
+                title:'imKey常见问题',
+                content:``,
+                children: [
+                    {
+                        id:'imKey_3_1',
+                        title:'imKey 硬件钱包是什么？',
+                        content:`
+                        <p>
+                        imKey Pro 是一款安全、好用、轻巧的去中心化硬件钱包，支持 ETH、BTC、COSMOS、EOS、TRX、CKB、BCH、LTC、KSM、DOT、FIL 在内的 11 条公链及其链上资产。此外，通过自定义节点功能，你可将 ETH 钱包快速切换到 Arbitrum 和 Optimism 等主流 Layer2 网络的钱包以及 BSC、Avalanche、Polygon 等主流 EVM 兼容链的钱包。
+</p>`
+                    },
+                    {
+                        id:'imKey_3_2',
+                        title:'如果我的 imKey 钱包丢失或被盗了怎么办？',
+                        content: `
+                        <p>
+                        imKey 钱包采用 PIN 码保护机制，如被他人连续输入五次错误 PIN 码，imKey 会被重置，钱包内所有敏感信息都会被清空（你的数字资产不受影响），从而保障钱包安全。
+硬件钱包丢失或被盗后，建议你尽快购买新的硬件钱包，通过「助记词」恢复资产。操作步骤可见：如果 imKey 丢失 / 损坏，如何恢复资产？ 
+</p>
+                        `
+                    },
+                    {
+                        id:'imKey_3_3',
+                        title:'售后服务政策？',
+                        content: `
+                        <p>
+                        由于硬件钱包是一款特殊的安全产品，为保障品质服务和降低供应链安全风险，我们支持 7 天内未拆封退换货和一年内非人为原因质量问题的换新服务，但无法提供任何维修服务。
+</p>`
+                    },
+                    {
+                        id:'imKey_3_4',
+                        title:'如何使用 imKey 硬件钱包？',
+                        content: `
+                        <p>
+                        点击查看 imKey 与 imToken 首次配对绑定 教程
+</p>`
+                    }
+                ]
+            },
+            {
+                id:'imKey-4',
+                title:'助记词密盒',
+                content:``,
+                children: [
+                    {
+                        id:'imKey-4-1',
+                        title:'密盒S1VS密盒P1',
+                        content:`
+                        <h4>密盒 S1：</h4>
+                <p>
+                高性价比的助记词保管工具，防水、防火、防腐蚀、防冲击且完全离线，隔离一切潜在风险；
+                <a href="https://shop42404838.m.youzan.com/wscgoods/detail/2xj16xca3nkmm?dc_ps=3340495314934140935.200001">更多了解</a>
+</p>
+<h4>密盒 P1：</h4>
+<p>
+多功能密钥管理器，可支持助记词、私钥及任一符合 ASCII 规范的密码存储，支持二次加密，具备密盒 S1 相同的物理性能，是高阶用户的优选方案。
+<a href="https://shop42404838.m.youzan.com/wscgoods/detail/36bm6keap5t1a33?dc_ps=3340498535169757189.200001">更多了解</a>
+</p>
+<img src="${imKey_4}" alt="imKey_4">
+`
+                    },
+                    {
+                        id:'imKey-4-2',
+                        title:'密盒 S1',
+                        content:``,
+                        children:[
+                                {
+                                    id:'imKey-4-2-1',
+                                    title:'产品概览',
+                                    content:`
+                                    <div class="table-container">
+                                     <table class="table product-table">
+                        <tr>
+                        <td rowspan="5">
+                        <img src="${imKey_4_2_1}" alt="密盒S1">
+</td>
+<td>产品名称</td>
+<td>密盒 S1</td>
+                        </tr>
+                        <tr>
+                        <td>产品描述</td>
+                        <td>密盒 S1 是一款高性价比的助记词保管工具，通体由 304 级不锈钢打造，AB 两面可以存放 2 套 12 位的助记词。与传统的纸质抄写相比，密盒 S1 具有防水、防火、防腐蚀的特性。</td>
+</tr>
+<tr>
+<td>产品应用</td>
+<td>离线保管助记词</td>
+</tr>
+<tr>
+<td>产品用途</td>
+<td>自用、礼品</td>
+</tr>
+<tr>
+<td>产品售价</td>
+<td>¥ 249 元</td>
+</tr>
+                        </table>
+                        </div>
+                                    `
+                                },
+                                {
+                                    id:'imKey-4-2-2',
+                                    title:'基本参数',
+                                    content: `
+                                    <div class="table-container">
+                                    <table class="table product-table">
+                        <tr>
+                        <td rowspan="4">
+                        <img src="${imKey_4_2_2}" alt="密盒S1参数">
+</td>
+<td>主体重量</td>
+<td>220g </td>
+                        </tr>
+                        <tr>
+                        <td>设备尺寸</td>
+                        <td>105mmx60mmx6mm</td>
+</tr>
+<tr>
+<td>产品材质</td>
+<td>304 不锈钢</td>
+</tr>
+<tr>
+<td>产品颜色</td>
+<td>不锈钢金属色</td>
+</tr>
+                        </table>
+                                    </div>
+                                    `
+                                },
+                                {
+                                    id:'imKey-4-2-3',
+                                    title: '包装清单',
+                                    content: `
+                                    <div class="table-container">
+                                    <table class="table product-table">
+                        <tr>
+                        <td rowspan="8">
+                        <img src="${imKey_4_2_3}" alt="密盒S1包装清单">
+</td>
+<td>包装重量</td>
+<td>412g </td>
+                        </tr>
+                        <tr>
+                        <td>包装尺寸</td>
+                        <td>145mmx120mmx40mm</td>
+</tr>
+<tr>
+<td rowspan="6">包装内容</td>
+<td>1 x  EVA 包装盒</td>
+</tr>
+<tr>
+<td>1 x 盒体</td>
+</tr>
+<tr>
+<td>1 x 字符块套装</td>
+</tr>
+<tr>
+<td>1 x 说明书</td>
+</tr>
+<tr>
+<td>1 x 螺丝刀</td>
+</tr>
+<tr>
+<td>3 x 备用螺丝</td>
+</tr>
+                        </table>
+                        <h1 style="text-align: center;font-size: 24px">Not Your Keys, Not Your Coins</h1>
+                        <h2>密盒 S1</h2>
+                        <h4>助记词尽在掌握，远离一切潜在风险</h4>
+</div>
+                                    `
+                                },
+                                {
+                                    id:'imKey-4-2-4',
+                                    title:'密盒特点',
+                                    content:`
+                                    <div class="table-container">
+                                    <table class="table product-table">
+                        <tr>
+                        <td><strong>防火</strong></td>
+                        <td rowspan="2"><img src="${imKey_4_2_4}" alt="miheS1防火"></td>
+                        </tr>
+                        <tr>
+                        <td>房屋火灾的平均温度为<br> 1500°F/815°C，而 imKey 密盒 S1 的熔点为 2548°F/1398°C，保护您的助记词免受房屋火灾。</td>
+</tr>
+
+<tr>
+                        <td><strong>防水耐腐蚀</strong></td>
+                        <td rowspan="2"><img src="${imKey_4_2_5}" alt="miheS1防水耐腐蚀"></td>
+                        </tr>
+                        <tr>
+                        <td>通体由 304 级不锈钢制成，防水，可抵抗各种类型的腐蚀。</td>
+</tr>
+
+<tr>
+                        <td><strong>防冲击</strong></td>
+                        <td rowspan="2"><img src="${imKey_4_2_6}" alt="miheS1防冲击"></td>
+                        </tr>
+                        <tr>
+                        <td>字母盖板与盒体间通过 6 个螺栓紧固，A / B 盒体通过卡扣形成整体，有效避免剧烈冲击时发生字母块移位。</td>
+</tr>
+
+<tr>
+                        <td><strong>可锁定防窥</strong></td>
+                        <td rowspan="2"><img src="${imKey_4_2_7}" alt="miheS1可锁定防窥"></td>
+                        </tr>
+                        <tr>
+                        <td>为了加强安全性，支持额外配锁，有效防窥。</td>
+</tr>
+
+<tr>
+                        <td><strong>易于使用，易于恢复</strong></td>
+                        <td rowspan="2"><img src="${imKey_4_2_8}" alt="miheS1易于使用_恢复"></td>
+                        </tr>
+                        <tr>
+                        <td>
+                        <ul>
+                        <li>符合 BIP39 协议。</li>
+                        <li>AB 两面可以存放 2 套 12 位的助记词。</li>
+</ul>
+</td>
+</tr>
+                        </table>
+</div>
+                                    `
+
+                                },
+                                {
+                                    id:'imKey-4-2-5',
+                                    title: '全面升级，安心之选',
+                                    content: `
+                                    <div class="table-container">
+                                    <table class="table product-table">
+                        <tr>
+                        <th>升级项目</th><th>原版</th><th>新版</th><th>升级说明</th>
+</tr>
+<tr>
+<td>字母块厚度</td>
+<td>0.4mm</td>
+<td>1mm</td>
+<td>增加安全性，防字母块错位，防冲击，高温不易发生变形</td>
+</tr>
+<tr>
+<td>字母块字符数</td>
+<td>单面 252 个</td>
+<td>双面 504 个</td>
+<td>避免出现缺少字符的偶发情况</td>
+</tr>
+<tr>
+<td>字符加工工艺</td>
+<td>蚀刻工艺</td>
+<td>激光工艺</td>
+<td>匹配 1mm 字母块的加工需要</td>
+</tr>
+<tr>
+<td>压板锁紧方式</td>
+<td>3 颗螺丝</td>
+<td>5 颗螺丝</td>
+<td>增加安全性，防冲击；</td>
+</tr>
+<tr>
+<td>AB 面卡扣</td>
+<td>1 个</td>
+<td>2 个</td>
+<td>增加安全性，防冲击；</td>
+</tr>
+<tr>
+<td>表面处理</td>
+<td>喷沙工艺</td>
+<td>拉丝工艺</td>
+<td>更美观</td>
+</tr>
+</table>
+</div>
+                                    `
+                                },
+                                {
+                                    id:'imKey-4-2-6',
+                                    title: '如何 使用/保管 密盒 ？',
+                                    content: `
+                                    <div class="table-container">
+                                    <h2>如何使用密盒 ？</h2>
+<table class="table product-table">
+<tr>
+<td><img src="${imKey_4_2_9}" alt="miheS1步骤1"></td>
+<td><img src="${imKey_4_2_10}" alt="miheS1步骤2"></td>
+<td><img src="${imKey_4_2_11}" alt="miheS1步骤3"></td>
+</tr>
+<tr>
+<td><img src="${imKey_4_2_12}" alt="miheS1步骤4"></td>
+<td><img src="${imKey_4_2_13}" alt="miheS1步骤5"></td>
+<td><img src="${imKey_4_2_14}" alt="miheS1步骤6"></td>
+</tr>
+</table>
+
+<table class="table product-table">
+<td><h3>如何保管密盒？</h3>
+<ul>
+<li>加锁，防窥</li>
+<li>保险箱保管，防盗</li>
+</ul>
+</td>
+<td><img src="${imKey_4_2_15}" alt="miheS1放入保险柜"></td>
+</tr>
+</table>
+</div>
+                                    `
+                                }
+
+                            ],
+                    }
+                ]
+            },
+            {
+                id:'imKey-4-3',
+                title:'密盒S1常见问题',
+                content:`
+               <div style="text-align: center">
+                <img src="${imKey_4_3_1}" alt="miheS1常见问题1">
+                <img src="${imKey_4_3_2}" alt="miheS1常见问题2">
+                <img src="${imKey_4_3_3}" alt="miheS1定制">
+</div>
+                <a href="https://shop42404838.m.youzan.com/wscgoods/detail/36e1hk8dzxijyq3?alias=36e1hk8dzxijyq3&banner_id=f.88725471~image_ad.8~0~omYZCja0&reft=1668583250609&spm=f.88725471">定制链接</a>
+                        `
             }
         ]
     },
